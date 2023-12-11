57ace76e858ad8e534985762ca752fc6c0d9101e
MalkaGreenberg <108021072+MalkaGreenberg@users.noreply.github.com>
2023-12-11 11:14:11 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,24 @@
 import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-// Create an HTTP link
-const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
-});
-
-// Set up authentication context if needed
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  if (!token) {
-    console.log("no token here! SORRY");
-  }
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-// Create an Apollo Client instance
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  uri: '/graphql',
   cache: new InMemoryCache(),
 });
+
+// // Set up authentication context if needed
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   if (!token) {
+//     console.log("no token here! SORRY");
+//   }
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
 
 // Rest of your imports...
 import './App.css';
